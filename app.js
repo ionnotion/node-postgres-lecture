0924ada65e157cae78b4e6827a34597c70a01ce8
ionnotion <adryan.rahadianto@gmail.com>
2023-04-19 10:38:30 +0700

    init repository

---
+++ app.js
@@ -0,0 +1,19 @@
+//! NPM
+//! node package manager
+
+//! https://node-postgres.com/
+
+//! initialize nmp di folder project
+
+//! buat gitignore untuk node_modules
+
+//! npm install package yang dibutuhkan
+
+//! foldering config dan migrations/setup
+//! https://node-postgres.com/apis/pool
+
+//! connection dan migration
+
+
+
+//! seeding data
